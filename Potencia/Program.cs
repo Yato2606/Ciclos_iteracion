eef75f586ae5e03fbf05c95caace724a2f9a0f12
Yato2606 <david.quevedo@uabc.edu.mx>
2022-04-08 21:54:45 -0700

    Agregar archivos de proyecto.

---
+++ Potencia/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Potencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, exponente, i;
+            double resultado = 1, resultadoNegativo;
+
+            Console.WriteLine("Dame la base: ");
+            numero = Console.ReadLine().Length;
+            Console.WriteLine("Ingresa el exponente: ");
+            exponente = Console.ReadLine().Length;
+
+            if (exponente<0)
+            {
+                exponente = exponente*-1;
+                i=1;
+                if (i<=exponente)
+                {
+                    resultado*=numero;
+                    i++;
+                }
+                else
+                {
+                    resultadoNegativo=(1/resultado);
+                    Console.WriteLine("{0}^{1}={2}", numero, exponente, resultadoNegativo);
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                i=1;
+                if (i<=exponente)
+                {
+                    resultado *=numero;
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("{0}^{1}={2}", numero, exponente, resultado);
+                    Console.ReadKey();
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Primos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, divisor, numDivisores = 0;
+
+            numero=2;
+
+            if (numero<=100)
+            {
+                divisor=1;
+
+                if (divisor<=numero)
+                {
+                    if (numero%divisor==0)
+                    {
+                        numDivisores =numDivisores+1;
+                    }
+                }
+            }
+            else
+            {
+                if (numDivisores<=2)
+                {
+                    Console.WriteLine("{0}", numero);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    numDivisores=0;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
